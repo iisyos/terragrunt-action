fb6b83e548de47d13f5e668111a46ad6710e7f7d
iisyos <kosuke1216a@yahoo.co.jp>
2024-04-06 18:35:24 +0900

    ci

--- src/main.sh
+++ src/main.sh
@@ -216,11 +216,9 @@ ${terragrunt_output}
 
   echo "tg_action_exit_code=${exit_code}" >> "${GITHUB_OUTPUT}"
 
-  local tg_action_output
-  tg_action_output=$(clean_multiline_text "${terragrunt_output}")
-  echo "tg_action_output=${tg_action_output}" >> "${GITHUB_OUTPUT}"
-  echo "tg_output=$(terragrunt output -json) | jq -c | jq -R" >> "${GITHUB_OUTPUT}"
-  terragrunt output -json | jq -c | jq -R
+  local tg_output
+  tg_output=$(terragrunt output -json 2>/dev/null | jq -c '.')
+  echo "tg_output=${tg_output}" >> "${GITHUB_OUTPUT}"
 }
 
 main "$@"
